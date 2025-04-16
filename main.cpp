813a9d958539925bec5ab822cbe9d8c6607a47fc
ZippyTheDoggy <47404219+ZippyTheDoggy@users.noreply.github.com>
2025-04-16 00:36:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 #include "./dll.h"
 
+void print_op(int a, int b, char op, int result) {
+    std::cout << "(a " << op << " b :: " << a << " " << op << " " << b << " = " << result << std::endl;
+}
+
 int main() {
     int a = 5;
     int b = 10;
-    int c = add(a, b);
 
-    std::cout << "a: " << a << std::endl;
-    std::cout << "b: " << b << std::endl;
-    std::cout << "c (a+b) >> " << c << std::endl;
+    print_op(a, b, '+', add(a, b));
+    print_op(a, b, '-', sub(a, b));
+    print_op(a, b, '*', mul(a, b));
+    print_op(a, b, '/', div(a, b));
+    
 }
